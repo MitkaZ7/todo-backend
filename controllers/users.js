90a2d56da9d66d888934f8cb9c8d35c9c4cab425
MitkaS7 <mitka.dev@gmail.com>
2022-06-21 14:44:35 +0300

    dev: add login func with jwt

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,17 @@
 import User from '../models/User.js'
 import Role from '../models/Role.js'
+import AuthError from '../errors/AuthError.js'
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 const saltRounds = 5;
+
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles
+  }
+  return jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: "24h"});
+}
 class UserController {
   async registration(req, res) {
     try {
@@ -26,7 +36,23 @@ class UserController {
   }
   async login(req, res) {
     try {
-
+      const {email, password} = req.body;
+      const user = await User.findOne({ email: email});
+      const matchedPass = await bcrypt.compare(password, user.password, async function(res,err){
+        if(err) {
+          console.log(err)
+        }
+      })
+      const userFound = User.findOne({
+        email: email,
+        password: matchedPass,
+      })
+      if(!userFound) {
+        return res.status(400).json({ message: 'User not found with email'})
+      } else {
+        const token = generateAccessToken(user._id, user.roles);
+        res.status(200).json({ token })
+      }
     } catch (e) {
       console.log(e);
       res.status(400).json({ message: 'Login failed' });
@@ -34,12 +60,7 @@ class UserController {
   }
   async getUsers(req, res) {
     try {
-      // временный код для сощания пары ролей в базе
-      // const userRole = new Role();
-      // const adminRole = new Role({value: "ADMINISTRATOR"});
-      // await userRole.save();
-      // await adminRole.save();
-        res.json("ssssss")
+
     } catch (e) {
 
     }
