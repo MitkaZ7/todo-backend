0e90469b45491f33952676575cd5bcb72d6d143a
MitkaS7 <mitka.dev@gmail.com>
2022-06-21 11:32:50 +0300

    dev: add registration function

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,46 @@
 import User from '../models/User.js'
 import Role from '../models/Role.js'
+import bcrypt from "bcrypt";
+const saltRounds = 5;
 class UserController {
   async registration(req, res) {
     try {
-
-    } catch (error) {
-
+      const { email, password } = req.body;
+      const candidate = await User.findOne({ email });
+      if (candidate) {
+        return res.status(400).json({ message:'Email already registered'})
+      }
+      const userRole = await Role.findOne({ value: "USER" })
+      await bcrypt.hash(password, saltRounds).then((hash) => User.create(
+        {
+          email,
+          password: hash,
+          roles: [userRole.value]
+        }
+      ));
+      return res.status(201).json({ message: 'Successfully registered'});
+    } catch(e) {
+        console.log(e);
+        res.status(400).json({message: 'Registration failed'});
     }
   }
   async login(req, res) {
     try {
 
-    } catch (error) {
-
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({ message: 'Login failed' });
     }
   }
   async getUsers(req, res) {
     try {
-      const userRole = new Role();
-      const adminRole = new Role({value: "ADMINISTRATOR"});
-      await userRole.save();
-      await adminRole.save();
+      // временный код для сощания пары ролей в базе
+      // const userRole = new Role();
+      // const adminRole = new Role({value: "ADMINISTRATOR"});
+      // await userRole.save();
+      // await adminRole.save();
         res.json("ssssss")
-    } catch (error) {
+    } catch (e) {
 
     }
   }
--- models/User.js
+++ models/User.js
@@ -1,18 +1,30 @@
 import {mongoose} from 'mongoose'
 const User = new mongoose.Schema({
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      default: 'Vincent Vega',
+      required: false,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      select: false,
     },
     roles: [{
       type: String,
       ref: 'Role'
-    }]
+    }],
+    avatar: {
+      type: String,
+      required: false,
+      default: 'https://disgustingmen.com/wp-content/uploads/2014/05/00001.mpls_snapshot_00.28.48_2013.05.03_11.12.18.png'
+    }
+
 
 })
 export default mongoose.model('User', User)
